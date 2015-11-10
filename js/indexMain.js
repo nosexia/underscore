3c2e8bee7437153b9ae800b5eb5b4361cfd1ec64
409903113@qq.com <409903113@qq.com>
2015-11-10 16:18:26 +0800

    _.pick应用在初始化view

--- js/indexMain.js
+++ js/indexMain.js
@@ -1,29 +1,10 @@
 define([
     'underscore'
 ],function(_){
-    var person = {
-        name: 'nose',
-        age: 23,
-        talk: function(){ console.log('i am talking') },
-        see: function(){ console.log('i am seeing')}
-    };
-
-    var result = _.pick(person, 'name', 'talk');
-    //console.log(result);   //{name: 'nose', talk: function(){ console.log('i am talking')}
-
-
-    //第二个参数是函数的情况
-
-    person = {
-        name: 'nose',
-        age: 23
-    };
-
-    var result = _.pick(person, function(val, attr){
-        return _.isNumber(val);
-    });
-    console.log(result);            //Object {age: 23}
-
+    var viewOptions = ['model', 'el'];
+    var options = {model: 'model1', collection: 'collection1'};
+    var result = _.pick(options, viewOptions);
+    console.log(result);            //model: 'model1'
 });
 
 
