b28cd5936e5019c01511fd10c4c982e83fd630fd
409903113@qq.com <409903113@qq.com>
2015-11-13 10:39:05 +0800

    _.any, _.some, 数组中有一项为true,断言成功，返回true

--- js/indexMain.js
+++ js/indexMain.js
@@ -2,14 +2,15 @@ define([
     'underscore'
 ],function(_){
 
-    var func = function(greeting){ 
-        return greeting + ': ' + this.name 
-    };
-    func = _.bind(func, {name: 'moe'}, 'hi');
-    console.log(func());        //hi: moe
-    //_.bind(func, {name: 'moe'}, 'h1')
-    // 方法{name: 'moe'}作为func的作用域
-    // 'hi' 为func的第一个arguments
+    //_.any, _.some方法，检测数组中，是否有一项不为false值
+    
+    var array1 = [null, undefined, false, 0];
+    var result = _.some(array1)  //false
+    console.log(result)
+    array1.unshift(1);
+    result = _.some(array1)    //true;
+    console.log(result)
+    //_.any, _.some方法，数组中有一项通过断言(数组中有一项为true)，返回true, 
 });
 
 
