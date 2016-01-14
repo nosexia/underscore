4f6290b23302f3ad2f41cc4a27457500d253b20c
nose <nose@futunn.com>
2016-01-14 10:28:40 +0800

    _.uniq方法

--- js/indexMain.js
+++ js/indexMain.js
@@ -1,11 +1,9 @@
 define([
     'underscore'
 ],function(_){
-    //_.difference，在数组array中排除(array和array2的公共项)
-    var array = [1 ,2,5];
-    var array2 = [2, 3, 4];
-    var result = _.difference(array, array2);
-    console.log(result);            //[1, 5]
+    var array1 = [1, 1, 2];
+    var array2 = _.uniq( array1 );
+    console.log(array2);
 });
 
 
